42d6e655df6875f016ac09801b89b6316d45168d
Zak Spence <zakspence@gmail.com>
2017-02-23 13:05:24 -0600

    added all song data

--- javascripts/v2.js
+++ javascripts/v2.js
@@ -4,6 +4,13 @@ var filter = function filter()
 {
 	var songs = [];
 	songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator*";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+songs.push("Love again (akinyele back) by Run the Jewels on the album Run the Jewels 2");
+songs.unshift("Downward Spiral by Danny Brown on the album Atrocity Exhibition*");
 
 	return {
 		filterNonAlphaNumeric ()
